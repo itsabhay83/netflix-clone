83ccfc9171e974c351edf1f72e2e6233ce9455f5
itsabhay83 <22bec003@nith.ac.in>
2025-03-09 23:12:29 +0530

    Created the tab-bar

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import {View , Text} from 'react-native';
 import {Link} from 'expo-router';
 
-export default function Index(){
+export default function Profile(){
     return(
         <View className="flex-1 justify-center items-center">
-        <Text className="text-5xl text-light-100 font-bold">Tab_section</Text>
+        <Text className="text-5xl text-light-100 font-bold">Profile Section</Text>
         </View>
     );
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,10 @@
 import {View , Text} from 'react-native';
 import {Link} from 'expo-router';
 
-export default function Index(){
+export default function Saved(){
     return(
         <View className="flex-1 justify-center items-center">
-        <Text className="text-5xl text-light-100 font-bold">Tab_section</Text>
+        <Text className="text-5xl text-light-100 font-bold">Saved Movies</Text>
         </View>
     );
 }
