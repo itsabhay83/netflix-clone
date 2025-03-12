45320a56fd3357585e434118920f81a67b22659a
itsabhay83 <22bec003@nith.ac.in>
2025-03-12 18:15:04 +0530

    modified the index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,85 @@
-import {View , Text  ,Image, ScrollView} from 'react-native';
-import {Link} from 'expo-router';
-import { images } from '@/constants/images';
-import { icons } from '@/constants/icons';
-import SearchBar from '@/components/SearchBar';
-import { useRouter } from 'expo-router';
-export default function Index(){
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    ScrollView,
+    Image,
+    FlatList,
+  } from "react-native";
+  import { useRouter } from "expo-router";
+  import { fetchMovies } from "@/services/api";  
+  import { icons } from "@/constants/icons";
+  import { images } from "@/constants/images";
+  
+  import SearchBar from "@/components/SearchBar";
+  import MovieCard from "@/components/MovieCard";
+  import TrendingCard from "@/components/TrendingCard";
+  import useFetch from "@/services/useFetch";
+  const Index = () => {
     const router = useRouter();
-    return(
-        <View className='flex-1 bg-primary'>
-            <Image source = {images.bg} className='absolute w-full z-0 '/>
-                <ScrollView className='flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight : "100%" ,
-                     paddingBottom: 10}}>
-                    <Image source = {icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto'/>
-                    <SearchBar
-                        onPress={() => router.push('/search')}
-                     placeholder='Search the Movie' 
-                    />
-                </ScrollView>
-        </View> 
+    const {
+      Data: movies,
+      loading: moviesLoading,
+      error: moviesError,
+    } = useFetch(() => fetchMovies({ query: "" }));
+  
+    return (
+      <View className="flex-1 bg-primary">
+        <Image
+          source={images.bg}
+          className="absolute w-full z-0"
+          resizeMode="cover"
+        />
+  
+        <ScrollView
+          className="flex-1 px-5"
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+        >
+          <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+  
+          {moviesLoading ? (
+            <ActivityIndicator
+              size="large"
+              color="#0000ff"
+              className="mt-10 self-center"
+            />
+          ) : moviesError  ? (
+            <Text>Error: {moviesError?.message}</Text>
+          ) : (
+            <View className="flex-1 mt-5">
+              <SearchBar
+                onPress={() => {
+                  router.push("/search");
+                }}
+                placeholder="Search for a movie"
+              />
+  
+              <>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">
+                  Latest Movies
+                </Text>
+  
+                <FlatList
+                  data={movies || null}
+                  renderItem={({ item }) => <MovieCard {...item} />}
+                  keyExtractor={(item) => item.id.toString()}
+                  numColumns={3}
+                  columnWrapperStyle={{
+                    justifyContent: "flex-start",
+                    gap: 20,
+                    paddingRight: 5,
+                    marginBottom: 10,
+                  }}
+                  className="mt-2 pb-32"
+                  scrollEnabled={false}
+                />
+              </>
+            </View>
+          )}
+        </ScrollView>
+      </View>
     );
-}
+  };
+  
+  export default Index;
